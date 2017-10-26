db03ab0cdcf926063f11f1ebef3c18e5aebab1f5
Alex <alex@appmake.com.br>
2017-10-26 08:48:21 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+        <meta charset="UTF-8">
+        <title> Biblioteca Online</title>
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
+        <!--Import jQuery before materialize.js-->
+        <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+        <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+        <div id="divCenter" class="container">
+            <div class="row">
+                <div class="col s12 m12">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="image/banner.jpg">
+                            <span id="image-title" class="card-title"
+                            style="font-weight: bold; font-size: 50px;">
+                                Biblioteca Online
+                            </span>
+                        </div>
+                        <div class="card-action">
+                            <a href="?pagina=novo">Novo Livro</a>
+                            <a href="?pagina=pesquisa">Pesquisar Livro</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+  </html>
